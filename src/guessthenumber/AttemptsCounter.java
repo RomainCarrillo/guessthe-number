5d9eeb24240c25b0d56e25529eccd987a47ae762
Romain Carrillo <carrillo.romain@gmail.com>
2021-10-02 12:49:17 +0200

    AJout Javadoc sur la classe AttemptsCounter

--- src/guessthenumber/AttemptsCounter.java
+++ src/guessthenumber/AttemptsCounter.java
@@ -1,13 +1,26 @@
 package guessthenumber;
 
+/**
+ * 
+ * <b>Gestion du compteur de tentatives</b>
+ * 
+ * @author RomainCarrillo
+ *
+ */
 public class AttemptsCounter {
 	private static int attemptsNumber;
 	private static String message = "Nombre d'essais : ";
 	
+	/**
+	 * Augmente la valeur de attemptsCounter à chaque appel
+	 */
 	public static void countAttempts() {
 		attemptsNumber++;
 	}
 	
+	/**
+	 * Affiche le nombre de tentatives du joueur
+	 */
 	public static void getAttempsNumber() {
 		System.out.println(message + attemptsNumber);
 	}
